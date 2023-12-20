d6e5f32db97fe079898dcd91aa9d59e916589e36
makaronma <87429019+makaronma@users.noreply.github.com>
2023-12-20 17:41:04 +0800

    output classified cropped

--- auto-classify.py
+++ auto-classify.py
@@ -4,19 +4,25 @@ from tqdm import tqdm
 from typing import Any
 import numpy.typing as npt
 import pickle
+import cv2
+import cv2.typing as cvt
 
+
+NUM_TIME_TO_SAMPLE: int = 1
+CHOSEN_MODEL = "cnn" # "cnn" or "hog" (cnn more accurate but slower)
 
 def recognize(image_path: str):
     face_image = face_recognition.load_image_file(image_path)
-    encodings = face_recognition.face_encodings(face_image)
+    face_locations = face_recognition.face_locations(face_image, NUM_TIME_TO_SAMPLE, CHOSEN_MODEL)
+    encodings = face_recognition.face_encodings(face_image, face_locations)
     num_face: int = len(encodings)  # type: ignore
-    face_locations = face_recognition.face_locations(face_image)
     return encodings, num_face, face_locations
 
 
 class ImgFace:
-    def __init__(self, img_path: str, encoding: npt.NDArray[Any], face_location: npt.NDArray[Any]):
+    def __init__(self, img_path: str, img_name: str, encoding: npt.NDArray[Any], face_location: npt.NDArray[Any]):
         self.img_path = img_path
+        self.img_name = img_name
         self.encoding = encoding
         self.face_location = face_location
 
@@ -26,15 +32,13 @@ class ImgFace:
 
 
 def main():
-    unknown_dir_name = "./all-unknown"
-
-    # key(index): (img_path, encodings)[]
+    UNKNOWN_DIR_NAME = "./all-unknown"
     cache_encoding_dict: dict[str, list[ImgFace]] = {}
     no_face_imgs = []
 
-    # Loop all imgs from unknown folder
-    for image_name in tqdm(os.listdir(unknown_dir_name)):
-        image_path = f"{unknown_dir_name}/{image_name}"
+    # ============== Loop all imgs from unknown folder ==============
+    for img_name in tqdm(os.listdir(UNKNOWN_DIR_NAME)):
+        image_path = f"{UNKNOWN_DIR_NAME}/{img_name}"
         encodings, num_face, face_locations = recognize(image_path)
 
         # Condition1: no face detected
@@ -45,7 +49,7 @@ def main():
         # any face detected
         # Loop all faces in one image
         for i in range(num_face):
-            currImgFace =  ImgFace(image_path, encodings[i], face_locations[i])
+            currImgFace = ImgFace(image_path, img_name, encodings[i], face_locations[i])  # type: ignore
             any_matched = False
 
             for i in range(len(cache_encoding_dict)):
@@ -74,8 +78,37 @@ def main():
             print(i, [item.img_path for item in curr_dict])
             print("===============")
 
+    # ============== Crop image with class ==============
+    for key, value in cache_encoding_dict.items():
+        if not len(value) > 1:
+            continue
+
+        dir_path = f"out/{key}"
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+
+        # select two image in class and crop it
+        for box_i in range(2):
+            box_data = value[box_i]
+            img_path = box_data.img_path
+            print(img_path)
+
+            # Read image
+            img = cv2.imread(img_path)
+
+            # Crop image with face location
+            top, right, bottom, left = box_data.face_location
+            face_img: cvt.MatLike = img[top:bottom, left:right]
+
+            # Copy cropped image to a new file
+            # create folder if not exist
+            
+            new_file_path = f"{dir_path}/cropped_class-{key}_{box_i}-{box_data.img_name}"
+            cv2.imwrite(new_file_path, face_img)
+
+            print(f"Cropped image saved: {new_file_path}")
+
 
 if __name__ == "__main__":
     main()
     # data: dict[str, list[ImgFace]] = pickle.load(open("out/encoding-dict.pkl", "rb"))
-    
