db62e2960c233cecbfb7c4d916319d1f5b4dedfd
makaronma <87429019+makaronma@users.noreply.github.com>
2023-12-20 16:51:16 +0800

    use class store data

--- auto-classify.py
+++ auto-classify.py
@@ -10,13 +10,13 @@ def recognize(image_path: str):
     face_image = face_recognition.load_image_file(image_path)
     encodings = face_recognition.face_encodings(face_image)
     num_face: int = len(encodings)  # type: ignore
-    return encodings, num_face
+    face_locations = face_recognition.face_locations(face_image)
+    return encodings, num_face, face_locations
 
 
-class ImgFaceWithClass:
-    def __init__(self, img_path: str, class_idx: int, encoding: npt.NDArray[Any], face_location: npt.NDArray[Any]):
+class ImgFace:
+    def __init__(self, img_path: str, encoding: npt.NDArray[Any], face_location: npt.NDArray[Any]):
         self.img_path = img_path
-        self.class_idx = class_idx
         self.encoding = encoding
         self.face_location = face_location
 
@@ -29,13 +29,13 @@ def main():
     unknown_dir_name = "./all-unknown"
 
     # key(index): (img_path, encodings)[]
-    cache_encoding_dict: dict[str, list[tuple[str, npt.NDArray[Any]]]] = {}
+    cache_encoding_dict: dict[str, list[ImgFace]] = {}
     no_face_imgs = []
 
     # Loop all imgs from unknown folder
     for image_name in tqdm(os.listdir(unknown_dir_name)):
         image_path = f"{unknown_dir_name}/{image_name}"
-        encodings, num_face = recognize(image_path)
+        encodings, num_face, face_locations = recognize(image_path)
 
         # Condition1: no face detected
         if not (num_face >= 1):
@@ -44,22 +44,23 @@ def main():
 
         # any face detected
         # Loop all faces in one image
-        for ec in encodings:
+        for i in range(num_face):
+            currImgFace =  ImgFace(image_path, encodings[i], face_locations[i])
             any_matched = False
 
             for i in range(len(cache_encoding_dict)):
-                known_ec = [tup[1] for tup in cache_encoding_dict[str(i)]]
-                results = face_recognition.compare_faces(known_ec, ec)
+                known_ec = [item.encoding for item in cache_encoding_dict[str(i)]]
+                results = face_recognition.compare_faces(known_ec, currImgFace.encoding)
 
                 # Condition2: if this face match current known encoding, then add this encoding
                 if all(r == True for r in results):
-                    cache_encoding_dict[str(i)] = [*cache_encoding_dict[str(i)], ((image_path, ec))]
+                    cache_encoding_dict[str(i)] = [*cache_encoding_dict[str(i)], currImgFace]
                     any_matched = True
                     break
 
             # Condition3: no match any in dict, then create one new category
             if any_matched == False:
-                cache_encoding_dict[str(len(cache_encoding_dict))] = [(image_path, ec)]
+                cache_encoding_dict[str(len(cache_encoding_dict))] = [currImgFace]
 
     with open("out/encoding-dict.pkl", "wb+") as fp:
         pickle.dump(cache_encoding_dict, fp)
@@ -70,9 +71,11 @@ def main():
         curr_dict = cache_encoding_dict[str(i)]
         num = len(curr_dict)
         if num >= 1:
-            print(i, [data[0] for data in curr_dict])
+            print(i, [item.img_path for item in curr_dict])
             print("===============")
 
 
 if __name__ == "__main__":
     main()
+    # data: dict[str, list[ImgFace]] = pickle.load(open("out/encoding-dict.pkl", "rb"))
+    
